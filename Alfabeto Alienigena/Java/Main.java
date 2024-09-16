6254845b1e8a81cfad3029ee8aed71fa556d8b75
Vitor <vitormeiragomes@outlook.com>
2024-09-16 15:23:43 -0300

    Alfabeto alienigena JAVA

---
+++ Alfabeto Alienigena/Java/Main.java
@@ -0,0 +1,56 @@
+//https://neps.academy/br/exercise/2792
+
+import java.util.*;
+
+public class Main
+{
+
+    static boolean presente(char letraMsg, String alfabeto, int alfabetoTAM)
+    {
+
+        for(int i = 0; i < alfabetoTAM; i++)
+        {
+            if(letraMsg == alfabeto.charAt(i))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner scanf = new Scanner(System.in);
+
+        int alfabetoTAM = scanf.nextInt();
+        int mensagemTAM = scanf.nextInt();
+        scanf.nextLine();
+
+        String alfabeto = scanf.nextLine();
+        String mensagem = scanf.nextLine();
+
+        boolean resp = true;
+
+        for(int i = 0; i < mensagemTAM; i++)
+        {
+            if(!presente(mensagem.charAt(i), alfabeto, alfabetoTAM))
+            {
+                resp = false;
+                i = mensagemTAM;
+            }
+        }
+
+        if(resp)
+        {
+            System.out.println("S");
+        }
+        else{
+            System.out.println("N");
+        }
+
+        
+
+        
+        
+    }
+}
