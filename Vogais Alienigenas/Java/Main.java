3a84218cc593268833756d20d1a882a735c1b224
Vitor <vitormeiragomes@outlook.com>
2024-09-18 16:00:03 -0300

    Finalizando vogais alienigenas

--- Vogais Alienigenas/Java/Main.java
+++ Vogais Alienigenas/Java/Main.java
@@ -3,4 +3,46 @@ import java.util.*;
 
 public class Main {
     
+    static int contandoRepetidos(char letra, String frase)
+    {
+        int tam = frase.length();
+        int contador = 0;
+
+
+        for(int i = 0; i < tam; i++)
+        {
+            if(letra == frase.charAt(i)) contador++;
+        }
+
+        return contador;
+    }
+    
+    
+    
+    
+    public static void main(String args[])
+    {
+        
+        Scanner scanf = new Scanner(System.in);
+
+        while(scanf.hasNext())
+        {
+            String letrasAlien = scanf.nextLine();
+
+            String frase = scanf.nextLine();
+
+            int contador = 0;
+
+            for(int i = 0; i < letrasAlien.length(); i++)
+            {
+                contador += contandoRepetidos(letrasAlien.charAt(i), frase);
+            }
+
+            System.out.println(contador);
+        }
+
+
+        scanf.close();
+        
+    }
 }
